0944210902cab93a9a215b64292658250512c5fc
Arturo <manolo@manolo.es>
2014-04-02 12:33:27 +0200

    Modificado método bounce, añadido for para añadir bolas al Array de bolas y darles caracterísitcas aleatorias

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -13,6 +14,7 @@ public class BallDemo
     private Canvas myCanvas;
     private BouncingBall[] myBalls;
     private int numberOfBalls;
+    private Random rnd;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -22,10 +24,11 @@ public class BallDemo
         myCanvas = new Canvas("Ball Demo", 600, 500);
         this.numberOfBalls = numberOfBalls;
         myBalls = new BouncingBall[numberOfBalls];
+        rnd = new Random();
     }
 
     /**
-     * Simulate two bouncing balls
+     * Simulate n bouncing balls
      */
     public void bounce()
     {
@@ -37,6 +40,11 @@ public class BallDemo
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
+        for (i=0;i<myBalls.length;i++){
+            
+            myBalls[i] = new BouncingBall()
+        
+        }
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
         BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
